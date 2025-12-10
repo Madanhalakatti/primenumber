de21694d924882eb0674b649cbdbb887221f314b
Madanhalakatti <153990042+Madanhalakatti@users.noreply.github.com>
2025-12-11 00:46:33 +0530

    Add files via upload

---
+++ primecheck.java
@@ -0,0 +1,36 @@
+
+
+    import java.util.Scanner;
+
+public class Primecheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+
+        // Handling numbers less than 2
+        if (num < 2) {
+            isPrime = false;
+        } else {
+            // Check divisors from 2 to sqrt(num)
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a Prime Number");
+        } else {
+            System.out.println(num + " is Not a Prime Number");
+        }
+
+        sc.close();
+    }
+}
+
